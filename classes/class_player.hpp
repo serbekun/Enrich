de84d2f51a5abf91088ee307640fd2da998192d6
serbekun <sergey994049@gmail.com>
2025-04-12 21:16:06 +0900

    Update class_player.hpp

--- classes/class_player.hpp
+++ classes/class_player.hpp
@@ -1,60 +1,100 @@
+#pragma once
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-namespace game_class {
+namespace GamePlayObject {
 
 class player {
 
-
-
-public:
-    
     string name;
     int money;
     int health;
     int stress; // min 0% max 100%
     int pills;
     int pills_ifactive;
+
+public:
     
     player() {
+        name = "player";
         money = 25000;
         health = 100;
         stress = 0;
         pills = 2;
+        pills_ifactive = 100;
     }
 
     int get_money_value() const {
         return money;
     }
 
-    void set_player_name(const string *set_name) {
-        name = *set_name;
+    int get_stress_value() const {
+        return stress;
+    }
+
+    int get_health_value() const {
+        return health;        
+    }
+
+    string get_player_name() {
+        return name;
+    }
+
+    void change_money_value(int change_value) {
+        this->money += change_value;
+    }
+
+    void change_money_value_almost(int change_value) {
+        this->money = change_value;
+    }
+
+    void change_pills_value(int change_value) {
+        this->pills += change_value;
+    }
+
+    void change_stress_value(int change_value) {
+        this->stress += change_value;
+    }
+
+    void change_stress_value_almost(int change_value) {
+        this->stress = change_value;
+    }
+
+    void change_health_value(int change_value) {
+        this->health += change_value;
+    }
+
+    void change_health_value_almost(int change_value) {
+        this->health = change_value;
+    }
+
+    void set_player_name(const string set_name) {
+        this->name = set_name;
     }
 
     void up_loss_pills_ifactive(int round_count) {
         if (round_count % 10 == 0) {
-            pills_ifactive += 2;
+            this->pills_ifactive += 2;
         }
-        
     }
 
     void loss_pills_ifactive() {
         pills_ifactive /= 2;
-        int minimizer = pills_ifactive /= 2;
-        pills_ifactive += minimizer;
+        int minimizer = pills_ifactive / 2;
+        this->pills_ifactive += minimizer;
     }
 
     void drink_pills() {
-        pills--;
-        stress -= pills_ifactive;
+        if (pills <= 0) {
+            cout << "you can't drink pills because you don't have pills!!!" << endl;
+            return;
+        }
+        this->pills--;
+        this->stress -= pills_ifactive;
         loss_pills_ifactive();
     }
-
     
-
-
 };
 
 }
