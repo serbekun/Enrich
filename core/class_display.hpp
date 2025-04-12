d443d504bceae2bb37425a20e315ea9fda584643
serbekun <sergey994049@gmail.com>
2025-04-12 21:18:37 +0900

    Create class_display.hpp

---
+++ core/class_display.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+
+// include "game play object"
+#include "../classes/class_player.hpp"
+#include "../classes/class_shop.hpp"
+
+using namespace std;
+
+namespace GameCore {
+
+class display {
+
+public:   
+
+    void show_base_info(GamePlayObject::shop shop, GamePlayObject::player player) {
+        cout << player.get_player_name() << " money - " << player.get_money_value() << endl;
+        cout << player.get_player_name() << " strees - " << player.get_stress_value() << endl;
+        cout << "1. drink pills" << endl;
+        cout << "2. buy pills" << endl;
+        cout << ":";
+    }
+
+    void massage_before_buy_pills() {
+        cout << "have many pills you want buy :";
+    }
+};
+
+} // name space
