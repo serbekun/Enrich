958a09ae83fb0ba56ea8b9a318be780b8303251f
serbekun <sergey994049@gmail.com>
2025-04-12 21:17:59 +0900

    Update class_CandleReader.hpp

--- core/class_CandleReader.hpp
+++ core/class_CandleReader.hpp
@@ -1,6 +1,10 @@
+#pragma once
+
 #include <iostream>
-#include <../classes/class_player.hpp>
-#include <../classes/class_shop.hpp>
+#include "../classes/class_player.hpp"
+#include "../classes/class_shop.hpp"
+
+using namespace std;
 
 namespace GameCore {
 
@@ -10,18 +14,18 @@ private:
     int normal_world_money;
     int normal_world_product;
 public:
-    CandleReader() = default;
+    CandleReader() : normal_world_money(0), normal_world_product(0) {}
 
     ~CandleReader() = default;
 
-    void set_normal_world_money(const player& player_obj, const shop& shop_obj) {
+    void set_normal_world_money(const GamePlayObject::player& player_obj,
+        const GamePlayObject::shop& shop_obj) {
         normal_world_money = player_obj.get_money_value() + shop_obj.get_money_value();
     }
     
-    int get_normal_world_money() {
+    int get_normal_world_money() const {
         return normal_world_money;
     }
-
 };
 
 }
