81fbc59884e18c677677c57db5d847137cd06635
serbekun <sergey994049@gmail.com>
2025-04-13 23:14:06 +0900

    Update class_shop.cpp

--- classes/class_shop.cpp
+++ classes/class_shop.cpp
@@ -1,8 +1,7 @@
 #pragma once
 
 #include "class_shop.hpp"
-#include "../core/class_CandleReader.hpp" // тут нужен полный include
-#include "class_shop.hpp"
+#include "../core/class_CandleReader.hpp"
 
 using namespace std;
 using namespace GamePlayObject;
@@ -10,5 +9,5 @@ using namespace GamePlayObject;
 void shop::change_shop_ifactive(GameCore::CandleReader* cr) {
     int world_normal_money = cr->get_normal_world_money();
     int coefficient = world_normal_money / money;
-    cout << "&%& - " << coefficient << endl;
+    this->shop_ifactive = coefficient;
 }
