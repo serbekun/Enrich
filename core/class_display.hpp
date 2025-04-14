563286972db6548048e77c60b6843f6d91a74006
serbekun <sergey994049@gmail.com>
2025-04-14 23:16:29 +0900

    Update class_display.hpp

--- core/class_display.hpp
+++ core/class_display.hpp
@@ -5,6 +5,7 @@
 // include "game play object"
 #include "../classes/class_player.hpp"
 #include "../classes/class_shop.hpp"
+#include "class_CandleReader.hpp"
 
 using namespace std;
 
@@ -14,7 +15,8 @@ class display {
 
 public:   
 
-    void show_base_info(GamePlayObject::shop shop, GamePlayObject::player player) {
+    void show_base_info(GamePlayObject::shop shop, GamePlayObject::player player, int round_count) {
+        cout << "turn - " << round_count << endl;
         cout << player.get_player_name() << " money - " << player.get_money_value() << endl;
         cout << player.get_player_name() << " strees - " << player.get_stress_value() << endl;
         cout << player.get_player_name() << " pills - " << player.get_pills_value() << endl;
@@ -26,6 +28,38 @@ public:
     void massage_before_buy_pills() {
         cout << "have many pills you want buy :";
     }
+
+    void massage_before_buy_wood() {
+        cout << "have many wood you want buy :";
+    }
+
+    void massage_before_buy_stone() {
+        cout << "have many stone you want buy :";
+    }
+
+    // funk for tests
+
+    void start_test_message() {
+        cout << "===========TEST START===========" << endl;
+    }
+    
+    void show_shop_info(GamePlayObject::shop shop) {
+        cout << "shop money - " << shop.get_money_value() << endl;
+        cout << "shop ifactive - " << shop.get_shop_ifactive_value() << endl;
+        cout << "shop wood - " << shop.get_wood_value() << endl;
+        cout << "shop stone -" << shop.get_stone_value() << endl;
+        cout << "shop have pills - " << shop.get_have_pills() << endl;
+        cout << "shop pills cost - " << shop.get_pills_cost() << endl;
+    }
+
+    void show_CandleReader_info(GameCore::CandleReader cr) {
+        cout << "world normal money - " << cr.get_normal_world_money() << endl;
+    }
+
+    void end_test_message(){
+        cout << "============TEST END============" << endl;
+    }
+
 };
 
 } // name space
