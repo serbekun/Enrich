b4321092c429a712d08adc8c8987321af5fba303
serbekun <sergey994049@gmail.com>
2025-04-14 23:17:23 +0900

    Update start_menu.hpp

--- menu/start_menu.hpp
+++ menu/start_menu.hpp
@@ -1,38 +1,58 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "play_menu.hpp"
 
 using namespace std;
 
 namespace Menus {
 
-void start_menu() {
-
+void start_menu(bool test_mod) {
+    string input_str;
     short input;
 
-    while(1) {
-    cout << "==========MENU==========" << endl;
-    cout << "1. start" << endl;
-    cout << "2. credits" << endl;
-    cout << "3. exit" << endl;
-    cout << "select option :";
+    while (true) {
+        cout << "==========MENU==========" << endl;
+        cout << "1. Start" << endl;
+        cout << "2. Credits" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Select option: ";
 
-    cin >> input;
-    
-    if (input == 1) {
-        cout << "==========START==========" << endl;
-        Menus::play_menu();
-    } else if (input == 2) {
-        cout << "director - Mishcenko Sergey" << endl;
-        cout << "author - Mishenko Sergey" << endl;
-        cout << "code - Mishcenko Sergey" << endl;
-    } else if (input == 3) {
-        return;
+        getline(cin, input_str);
+
+        if (input_str.empty()) {
+            cout << "Nothing entered. Please enter a number from 1 to 3." << endl;
+            continue;
+        }
+
+        stringstream ss(input_str);
+        if (!(ss >> input) || !ss.eof()) {
+            cout << "Invalid input. Please enter a number from 1 to 3." << endl;
+            continue;
+        }
+
+        switch (input) {
+            case 1:
+                cout << "==========START==========" << endl;
+                Menus::play_menu(test_mod);
+                break;
+
+            case 2:
+                cout << "Director - Mishcenko Sergey" << endl;
+                cout << "Author - Mishenko Sergey" << endl;
+                cout << "Code - Mishcenko Sergey" << endl;
+                break;
+
+            case 3:
+                return;
+
+            default:
+                cout << input << " - Unknown option." << endl;
+                break;
+        }
     }
-
-    }
-
 }
 
 }
